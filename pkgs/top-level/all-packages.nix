d32db8871c296684dacdf6806a6abd3ac544171e
GenericNerdyUsername <genericnerdyusername@proton.me>
2022-10-18 23:17:38 +0100

    jetbrains-jdk: 17.0.3-b469.37 -> 17.0.4.1-b629.2, add JCEF support

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -28974,6 +28974,7 @@ with pkgs;
     jdk = jetbrains.jdk;
   }) // {
     jdk = callPackage ../development/compilers/jetbrains-jdk {  };
+    jcef = callPackage ../development/compilers/jetbrains-jdk/jcef.nix { };
   });
 
   jmusicbot = callPackage ../applications/audio/jmusicbot { };
